865426f0da891e98a5da92e1115d5187bde64347
justmars <39538958+justmars@users.noreply.github.com>
2023-01-04 21:01:05 +0800

    feat: fix code_mp_fts_id and statute_mp_fts_id queries

--- queries/x/code_mp_fts_id.sql
+++ queries/x/code_mp_fts_id.sql
@@ -1,22 +1,83 @@
+WITH rowids_match_q AS (
+  SELECT
+    ROW_NUMBER() over (
+      ORDER BY
+        cx.id
+    ) rn,
+    cx.id row_idx,
+    COUNT(*) over () max_count
+  FROM
+    lex_tbl_codification_fts_units cx
+    JOIN lex_tbl_codification_fts_units_fts
+    ON cx.rowid = lex_tbl_codification_fts_units_fts.rowid
+  WHERE
+    cx.codification_id = :code_id
+    AND lex_tbl_codification_fts_units_fts match escape_fts(:q)
+),
+rowids_match_range AS (
+  SELECT
+    rn,
+    row_idx,
+    max_count
+  FROM
+    rowids_match_q
+  WHERE
+    rn BETWEEN CAST(
+      :start AS INTEGER
+    )
+    AND CAST(
+      :end AS INTEGER
+    )
+),
+snippet_data AS (
+  SELECT
+    snippet(
+      lex_tbl_codification_fts_units_fts,
+      0,
+      '<mark>',
+      '</mark>',
+      '...',
+      15
+    ) matched_text
+  FROM
+    lex_tbl_codification_fts_units cx3
+    JOIN lex_tbl_codification_fts_units_fts
+    ON lex_tbl_codification_fts_units_fts.rowid = cx3.rowid
+  WHERE
+    cx3.id = cx2.id
+    AND lex_tbl_codification_fts_units_fts match escape_fts(:q)
+)
 SELECT
-  cx.id,
-  cx.material_path,
-  snippet(
-    lex_tbl_codification_fts_units_fts,
-    0,
-    '<mark>',
-    '</mark>',
-    '...',
-    15
-  ) matched_text
+  (
+    SELECT
+      rn
+    FROM
+      rowids_match_range
+    WHERE
+      row_idx = cx2.id
+  ) row_num,
+  cx2.id,
+  cx2.material_path,
+  (
+    SELECT
+      matched_text
+    FROM
+      snippet_data
+  ) snippet,
+  (
+    SELECT
+      max_count
+    FROM
+      rowids_match_range
+    LIMIT
+      1
+  ) max_count
 FROM
-  lex_tbl_codification_fts_units cx
-  JOIN lex_tbl_codification_fts_units_fts
-  ON cx.rowid = lex_tbl_codification_fts_units_fts.rowid
-  JOIN lex_tbl_codifications C
-  ON C.id = cx.codification_id
+  lex_tbl_codification_fts_units cx2
 WHERE
-  cx.codification_id = :code_id
-  AND lex_tbl_codification_fts_units_fts match escape_fts(:q)
-ORDER BY
-  cx.id
+  cx2.id IN (
+    SELECT
+      row_idx
+    FROM
+      rowids_match_range
+  )
--- queries/x/statute_mp_fts_id.sql
+++ queries/x/statute_mp_fts_id.sql
@@ -1,22 +1,83 @@
+WITH rowids_match_q AS (
+  SELECT
+    ROW_NUMBER() over (
+      ORDER BY
+        sx.id
+    ) rn,
+    sx.id row_idx,
+    COUNT(*) over () max_count
+  FROM
+    lex_tbl_statute_fts_units sx
+    JOIN lex_tbl_statute_fts_units_fts
+    ON sx.rowid = lex_tbl_statute_fts_units_fts.rowid
+  WHERE
+    sx.statute_id = :statute_id
+    AND lex_tbl_statute_fts_units_fts match escape_fts(:q)
+),
+rowids_match_range AS (
+  SELECT
+    rn,
+    row_idx,
+    max_count
+  FROM
+    rowids_match_q
+  WHERE
+    rn BETWEEN CAST(
+      :start AS INTEGER
+    )
+    AND CAST(
+      :end AS INTEGER
+    )
+),
+snippet_data AS (
+  SELECT
+    snippet(
+      lex_tbl_statute_fts_units_fts,
+      0,
+      '<mark>',
+      '</mark>',
+      '...',
+      15
+    ) matched_text
+  FROM
+    lex_tbl_statute_fts_units sx3
+    JOIN lex_tbl_statute_fts_units_fts
+    ON lex_tbl_statute_fts_units_fts.rowid = sx3.rowid
+  WHERE
+    sx3.id = sx2.id
+    AND lex_tbl_statute_fts_units_fts match escape_fts(:q)
+)
 SELECT
-  sx.id,
-  sx.material_path,
-  snippet(
-    lex_tbl_statute_fts_units_fts,
-    0,
-    '<mark>',
-    '</mark>',
-    '...',
-    15
-  ) matched_text
+  (
+    SELECT
+      rn
+    FROM
+      rowids_match_range
+    WHERE
+      row_idx = sx2.id
+  ) row_num,
+  sx2.id,
+  sx2.material_path,
+  (
+    SELECT
+      matched_text
+    FROM
+      snippet_data
+  ) snippet,
+  (
+    SELECT
+      max_count
+    FROM
+      rowids_match_range
+    LIMIT
+      1
+  ) max_count
 FROM
-  lex_tbl_statute_fts_units sx
-  JOIN lex_tbl_statute_fts_units_fts
-  ON sx.rowid = lex_tbl_statute_fts_units_fts.rowid
-  JOIN lex_tbl_statutes s
-  ON s.id = sx.statute_id
+  lex_tbl_statute_fts_units sx2
 WHERE
-  sx.statute_id = :statute_id
-  AND lex_tbl_statute_fts_units_fts match escape_fts(:q)
-ORDER BY
-  sx.id
+  sx2.id IN (
+    SELECT
+      row_idx
+    FROM
+      rowids_match_range
+  )
