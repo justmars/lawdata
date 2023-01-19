291e44c365936ce11e30ffff734ba0d675a43957
justmars <39538958+justmars@users.noreply.github.com>
2023-01-19 12:14:38 +0800

    feat: added case ponencia detail

--- plugins/advance_fts.py
+++ plugins/advance_fts.py
@@ -7,10 +7,10 @@ _escape_fts_re = re.compile(r'\s+|(".*?")')
 
 def set_tokens(q: str):
     """
-    This is datasette's [escape_fts()](https://github.com/simonw/datasette/blob/8e18c7943181f228ce5ebcea48deb59ce50bee1f/datasette/utils/__init__.py#L818-L829.) # noqa: E501
+    This is datasette's [escape_fts()](https://github.com/simonw/datasette/blob/8e18c7943181f228ce5ebcea48deb59ce50bee1f/datasette/utils/__init__.py#L818-L829.)
 
     For tokens that do not have double quotes, add a double quote.
-    """
+    """  # noqa: E501
     # If query has unbalanced ", add one at end
     if q.count('"') % 2:
         q += '"'
