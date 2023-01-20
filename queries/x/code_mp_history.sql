9fd2a53cee0b179ad5af1e84cc0201e755c924db
justmars <39538958+justmars@users.noreply.github.com>
2023-01-20 10:03:23 +0800

    feat: added history query for codifications

---
+++ queries/x/code_mp_history.sql
@@ -0,0 +1,12 @@
+SELECT
+  tbl.units -> t.path -> 'history' -- extract the path of the json tree, then extract the history component of that path
+FROM
+  lex_tbl_codifications tbl,
+  json_tree(
+    tbl.units,
+    '$'
+  ) t
+WHERE
+  tbl.id = :code_id
+  AND t.key = 'id' -- each item in the json tree was given an `id`
+  AND t.value = :mp -- that `id` refers to a material path
