2338b503f55f1bc1ea67cc1dd06dfd30dd12d593
justmars <39538958+justmars@users.noreply.github.com>
2023-06-01 16:50:03 +0800

    feat: bump

--- app/scripts/run.sh
+++ app/scripts/run.sh
@@ -4,10 +4,6 @@ set -e
 echo "Main SC database:"
 python -m app x-restore-db
 
-sqlite3 data/x.db 'PRAGMA journal_size_limit = 6144000;'
-sqlite3 data/x.db 'PRAGMA busy_timeout = 5000;'
-sqlite3 data/x.db 'PRAGMA synchronous = NORMAL;'
-
 # Run datasette
 datasette serve --immutable data/x.db \
   --host 0.0.0.0 \
